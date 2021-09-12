f87ce507014e795e62dcc2b00f5441e753ed6406
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-09-12 18:26:17 +0200

    add possibility to show a notification when preventing sleep

--- src/dev/hashnode/bas/SleepBlocker.java
+++ src/dev/hashnode/bas/SleepBlocker.java
@@ -1,8 +1,13 @@
 // Copyright 2020-2021 Bas Leijdekkers Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package dev.hashnode.bas;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationGroupManager;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.impl.CoreProgressManager;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.util.ReflectionUtil;
 import com.intellij.util.containers.ConcurrentLongObjectMap;
 
@@ -13,6 +18,7 @@ abstract class SleepBlocker implements Runnable {
     protected static final Logger LOG = Logger.getInstance("#insomniac");
 
     private boolean shouldPreventSleep = false;
+    private Notification notification;
 
     @Override
     public void run() {
@@ -31,9 +37,29 @@ abstract class SleepBlocker implements Runnable {
         final boolean hasLongRunningTask = isProgressBarActive();
         if (shouldPreventSleep) {
             shouldPreventSleep = hasLongRunningTask;
+            showHideNotification(shouldPreventSleep);
             handleSleep(shouldPreventSleep);
         } else if (hasLongRunningTask) {
             shouldPreventSleep = true;
+        }
+    }
+
+    private void showHideNotification(boolean show) {
+        if (!Registry.is("insomniac.show.notification")) {
+            return;
+        }
+        if (show) {
+            if (notification != null) {
+                return;
+            }
+            notification = NotificationGroupManager.getInstance()
+                    .getNotificationGroup("Insomniac")
+                    .createNotification("Long running task detected, preventing sleep", NotificationType.INFORMATION);
+            notification.notify(null);
+        }
+        else if (notification != null) {
+            notification.expire();
+            notification = null;
         }
     }
 
